16b5fd8de51581cd2128e10e120fe4605ffdb9e1
nidhijangid9 <jangidnidhi243@gmail.com>
2025-02-15 09:15:00 +0530

    Facial Expression on Home Page

--- mentorai/mentorai/mentorai/analysis/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import StudentProgress
-
-class StudentProgressSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StudentProgress
-        fields = '__all__'
